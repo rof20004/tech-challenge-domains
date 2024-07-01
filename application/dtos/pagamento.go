9bbeb0392e4ba36e964298cd2e5df3cd8a588d35
Rodolfo do Nascimento Azevedo <rof20004@gmail.com>
2024-07-01 13:21:48 -0300

    refactor: removido atributos desnecessários no NotificarPagamentoDto

--- application/dtos/pagamento.go
+++ application/dtos/pagamento.go
@@ -1,11 +1,5 @@
 package dtos
 
-import (
-	"time"
-
-	"github.com/rof20004/tech-challenge-domains/application/domains/pagamento"
-)
-
 type CartaoDto struct {
 	Bandeira    string `json:"bandeira"`
 	Numero      string `json:"numero"`
@@ -27,12 +21,5 @@ type CriarPagamentoDto struct {
 }
 
 type NotificarPagamentoDto struct {
-	Id            string                    `json:"id"`
-	PedidoId      string                    `json:"pedidoId"`
-	ValorTotal    int64                     `json:"valorTotal"`
-	Status        pagamento.StatusPagamento `json:"status"`
-	TransactionId string                    `json:"transactionId"`
-	Erro          string                    `json:"erro"`
-	EmailCliente  string                    `json:"emailCliente"`
-	CriadoEm      time.Time                 `json:"criadoEm,omitempty"`
+	PedidoId string `json:"pedidoId"`
 }
