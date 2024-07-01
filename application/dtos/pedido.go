e7ff0374ba1e816acb4817598b2cc9bc0f016287
Rodolfo do Nascimento Azevedo <rof20004@gmail.com>
2024-07-01 18:55:17 -0300

    feat: adicionado AtualizarPedidoDto

--- application/dtos/pedido.go
+++ application/dtos/pedido.go
@@ -2,6 +2,7 @@ package dtos
 
 import (
 	"github.com/rof20004/tech-challenge-domains/application/domains/cliente"
+	"github.com/rof20004/tech-challenge-domains/application/domains/pedido"
 	"github.com/rof20004/tech-challenge-domains/application/domains/produto"
 )
 
@@ -9,4 +10,9 @@ type CriarPedidoDto struct {
 	Cliente  cliente.Cliente   `json:"cliente"`
 	Produtos []produto.Produto `json:"produtos"`
 	Cartao   CartaoDto         `json:"cartao"`
+}
+
+type AtualizarPedidoDto struct {
+	PedidoId string              `json:"pedidoId"`
+	Status   pedido.StatusPedido `json:"status"`
 }
